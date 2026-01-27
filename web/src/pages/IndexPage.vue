239a4bcbfbd15d2f78adf33375f2edfba13902a7
rafael <rafaro@gmail.com>
2026-01-27 12:13:30 -0300

    chore: update favicons and resize landing logo

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/deigmata.png" style="width: 200px; height: 200px" />
+    <img alt="Quasar logo" src="~assets/deigmata.png" style="width: 50%; height: 50%" />
   </q-page>
 </template>
 
